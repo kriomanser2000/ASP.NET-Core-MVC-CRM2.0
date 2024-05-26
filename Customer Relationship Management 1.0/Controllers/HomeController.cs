5d6aaa2ddd0d9988fb9df1111cf638cdb9d98468
kriomanser2000 <kuzmenkasana05@gmail.com>
2024-05-26 06:48:56 +0300

    added entrance admin + main page

--- Customer Relationship Management 1.0/Controllers/HomeController.cs
+++ Customer Relationship Management 1.0/Controllers/HomeController.cs
@@ -30,6 +30,10 @@ namespace Customer_Relationship_Management_1._0.Controllers
         {
             return View();
         }
+        public IActionResult MainPage()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
