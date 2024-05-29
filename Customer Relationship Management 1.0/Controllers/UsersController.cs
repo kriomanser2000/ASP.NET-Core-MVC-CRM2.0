5f72f8b280fd6fb276fcb074c5f4eb18c6ac9095
kriomanser2000 <kuzmenkasana05@gmail.com>
2024-05-29 13:21:36 +0300

    deleting in Users working, editing not working

--- Customer Relationship Management 1.0/Controllers/UsersController.cs
+++ Customer Relationship Management 1.0/Controllers/UsersController.cs
@@ -18,5 +18,75 @@ namespace Customer_Relationship_Management_1._0.Controllers
             var users = _context.Users.ToList();
             return View(users);
         }
+
+        [HttpGet]
+        [Route("Users/Edit/{id}")]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("UserId,FirstName,Country,PhoneNumber,Email,IsApproved,Role")] User user)
+        {
+            if (id != user.UserId)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(user);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserExists(user.UserId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+
+        [HttpGet]
+        [Route("Users/Delete/{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UserExists(int id)
+        {
+            return _context.Users.Any(e => e.UserId == id);
+        }
     }
 }
