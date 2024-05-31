e58f4dc34c712fee0dc3838b5a1900733d8fb422
kriomanser2000 <kuzmenkasana05@gmail.com>
2024-05-31 19:27:30 +0300

    added more

---
+++ Customer Relationship Management 1.0/Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using Customer_Relationship_Management_1._0.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Customer_Relationship_Management_1._0.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LoginController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Json(new { success = false, message = "Invalid login data" });
+            }
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == model.Email && u.Password == model.Password);
+            if (user == null)
+            {
+                return Json(new { success = false, message = "Invalid login attempt" });
+            }
+            // Авторизувати користувача
+            return Json(new { success = true, role = user.Role });
+        }
+    }
+}
---
+++ Customer Relationship Management 1.0/Models/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Customer_Relationship_Management_1._0.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
