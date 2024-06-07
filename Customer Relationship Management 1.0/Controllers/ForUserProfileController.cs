ffb0930a853812448141ea9a67dca4113500b95b
kriomanser2000 <kuzmenkasana05@gmail.com>
2024-06-07 06:42:05 +0300

    edit users fir Admin fully worked

--- Customer Relationship Management 1.0/Controllers/ForUserProfileController.cs
+++ Customer Relationship Management 1.0/Controllers/ForUserProfileController.cs
@@ -6,29 +6,11 @@ using System.Security.Claims;
 
 namespace Customer_Relationship_Management_1._0.Controllers
 {
-    [Authorize]
     public class ForUserProfileController : Controller
     {
-        private readonly ApplicationDbContext _context;
-
-        public ForUserProfileController(ApplicationDbContext context)
+        public IActionResult Index()
         {
-            _context = context;
-        }
-
-        public async Task<IActionResult> Index()
-        {
-            var userId = GetCurrentUserId();
-            var user = await _context.Users.FirstOrDefaultAsync(u => u.UserId == userId);
-            if (user == null)
-            {
-                return NotFound();
-            }
-            return View(user);
-        }
-        private int GetCurrentUserId()
-        {
-            return int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            return View();
         }
     }
 }
