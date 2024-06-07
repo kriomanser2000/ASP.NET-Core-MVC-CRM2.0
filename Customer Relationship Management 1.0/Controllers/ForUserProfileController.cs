ba2a9f7604693cecceb6a6eaaa8314fbc5539348
kriomanser2000 <kuzmenkasana05@gmail.com>
2024-06-07 07:10:56 +0300

    need Show Profile Info for User

--- Customer Relationship Management 1.0/Controllers/ForUserProfileController.cs
+++ Customer Relationship Management 1.0/Controllers/ForUserProfileController.cs
@@ -3,14 +3,37 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace Customer_Relationship_Management_1._0.Controllers
 {
+    [Authorize]
     public class ForUserProfileController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public ForUserProfileController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            // Отримання ідентифікатора користувача із токена
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (userId == null)
+            {
+                return NotFound("User not found");
+            }
+            // Отримання інформації про користувача із бази даних
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.UserId.ToString() == userId);
+
+            if (user == null)
+            {
+                return NotFound("User not found");
+            }
+            return View(user);
         }
     }
 }
